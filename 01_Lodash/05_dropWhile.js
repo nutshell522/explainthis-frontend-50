6de456139e8be58f6516c0c3301626bbad1e97f6
TedYin <jjboy830227@gmail.com>
2026-03-21 23:13:53 +0800

    feat: finish dropWhile

--- 01_Lodash/05_dropWhile.js
+++ 01_Lodash/05_dropWhile.js
@@ -1,13 +1,19 @@
 /**
 請實作一個 dropWhile 函式。此函式接受兩個參數，第一個是參數是一個陣列，它可以是任何類型的陣列；第二個是一個 predicate 函式，會接受陣列中的元素，如果返回為真，則表示該元素應該被丟棄，直到返回的不為真則停止。
  */
-function dropWhile(array, predicate) {}
+function dropWhile(array, predicate) {
+  const result = [];
+  array.forEach((element) => {
+    if (!predicate(element)) result.push(element);
+  });
+  return result;
+}
 
 // 範例一
-console.log(dropWhile([1, 2, 3, 4], (n) => n < 3)); // [3, 4]
+console.log(dropWhile([1, 2, 3, 4, 5, 6], (value) => value < 4)); // => [4, 5, 6]
 
 // 範例二
-console.log(dropWhile([1, 2, 3, 4], (n) => n > 0)); // [1, 2, 3, 4]
+console.log(dropWhile([0, 1, 2], (value) => value < 5)); // => []
 
 // 範例三
-console.log(dropWhile([1, 6, 3, 4], (n) => n < 3)); // [6, 3, 4]
+console.log(dropWhile([0, 6, 1, 2], (value) => value < 1)); // => [6, 1, 2]
