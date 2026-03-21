8a4dd11e0e790c73483fdf22de710097422f76ca
TedYin <jjboy830227@gmail.com>
2026-03-21 23:35:35 +0800

    chore: fix the dropWhile

--- 01_Lodash/05_dropWhile.js
+++ 01_Lodash/05_dropWhile.js
@@ -2,11 +2,15 @@
 請實作一個 dropWhile 函式。此函式接受兩個參數，第一個是參數是一個陣列，它可以是任何類型的陣列；第二個是一個 predicate 函式，會接受陣列中的元素，如果返回為真，則表示該元素應該被丟棄，直到返回的不為真則停止。
  */
 function dropWhile(array, predicate) {
-  const result = [];
-  array.forEach((element) => {
-    if (!predicate(element)) result.push(element);
-  });
-  return result;
+  let index = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (!predicate(array[i])) {
+      break;
+    }
+    index++;
+  }
+  console.log(index);
+  return array.slice(index);
 }
 
 // 範例一
@@ -16,4 +20,4 @@ console.log(dropWhile([1, 2, 3, 4, 5, 6], (value) => value < 4)); // => [4, 5, 6
 console.log(dropWhile([0, 1, 2], (value) => value < 5)); // => []
 
 // 範例三
-console.log(dropWhile([0, 6, 1, 2], (value) => value < 1)); // => [6, 1, 2]
+console.log(dropWhile([0, 6, 1, 2], (value) => value < 5)); // => [6, 1, 2]
